51348c8b7b3124b17cfa4a0a620f554b86d992c0
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-07-03 09:56:10 +1200

    calculating price

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-// $("#cardsList").hide();
+$("#cardsList").hide();
+$("#details").hide();
 var locations = [
   {
     placeName: 'national',
     accom: [
       {
       id:1,
-      type:"hostel",
+      type:"Hostel",
       name: "National Park Backpackers",
       price: 30,
-      valid: {
-        minGuest:1,
-        minNight:1,
-        maxNight:10
-      },
+      minGuest:1,
+      maxGuest:1,
+      minNight:1,
+      maxNight:10,
       amenities: ["kitchen","Shared lounge"],
       img: "nationalHostel.jpg",
       morePhoto: ["nationalHostel2.jpg","nationalHostel3.jpg"],
@@ -24,13 +24,12 @@ var locations = [
     {
       id:2,
       type:"Motel",
-      name: "Pipers Lodge",
+      name: "National Park Pipers Lodge",
       price: 90,
-      valid: {
-        minGuest: 2,
-        minNight:3,
-        maxNight:10,
-      },
+      minGuest: 2,
+      maxGuest:4,
+      minNight:3,
+      maxNight:10,
       amenities: ["kitchen","Shared lounge"],
       img: "nationalMotel.jpg",
       morePhoto: ["nationalMotel2.jpg","nationalMotel3.jpg"],
@@ -44,9 +43,10 @@ var locations = [
       type:"Hotel",
       name: "The Park Hotel Ruapehu",
       price: 157,
-      valid: {
-
-      },
+      minGuest:1,
+      maxGuest:2,
+      minNight:1,
+      maxNight:5,
       amenities: ["kitchen","Shared lounge"],
       img: "nationalHotel.jpg",
       morePhoto: ["nationalHotel2.jpg","nationalHotel3.jpg"],
@@ -58,16 +58,15 @@ var locations = [
     {
       id:4,
       type:"House",
-      name: "Donlouin Cottage",
+      name: "National Park Donlouin Cottage",
       price: 240,
-      valid: {
-        minGuest: 1,
-        minNight:2,
-        maxNight:15
-      },
+      minGuest:1,
+      maxGuest:4,
+      minNight:2,
+      maxNight:15,
       amenities: ["kitchen","Shared lounge"],
       img: "nationalHouse.jpg",
-      morePhoto: ["nationalHotel2.jpg","nationalHotel3.jpg"],
+      morePhoto: ["nationalHouse2.jpg","nationalHouse3.jpg"],
       add: "28 Findlay St, National Park, 3989 National Park, New Zealand",
       lat: -39.175000,
       lng: 175.399680,
@@ -80,14 +79,13 @@ var locations = [
     accom: [
       {
       id:1,
-      type:"hostel",
+      type:"Hostel",
       name: "Whakapapa Holiday Park",
       price: 30,
-      valid: {
-        minGuest: 1,
-        minNight:1,
-        maxNight:10
-      },
+      minGuest:1,
+      maxGuest:1,
+      minNight:1,
+      maxNight:10,
       amenities: ["kitchen","Shared lounge"],
       img: "whakapapaHostel.jpg",
       img: "nationalHouse.jpg",
@@ -102,11 +100,10 @@ var locations = [
       type:"Motel",
       name: "Skotel Alpine Resort",
       price: 90,
-      valid: {
-        minGuest: 2,
-        minNight:3,
-        maxNight:10
-      },
+      minGuest: 2,
+      maxGuest:4,
+      minNight:3,
+      maxNight:10,
       amenities: ["kitchen","Shared lounge"],
       img: "whakapapaMotel.jpg",
       morePhoto: ["whakapapaHotel2.jpg","whakapapaHotel3.jpg"],
@@ -118,13 +115,12 @@ var locations = [
     {
       id:3,
       type:"Hotel",
-      name: "Chateau Hotel",
+      name: "Whakapapa Chateau Hotel",
       price: 157,
-      valid: {
-        minGuest: 1,
-        minNight:1,
-        maxNight:5
-      },
+      minGuest: 1,
+      maxGuest:2,
+      minNight:1,
+      maxNight:5,
       amenities: ["kitchen","Shared lounge"],
       img: "whakapapaHotel.jpg",
       add: "State Highway 48 Whakapapa Village, Mt Ruapehu, Tongariro National Park 3951",
@@ -135,15 +131,14 @@ var locations = [
     {
       id:4,
       type:"House",
-      name: "Whakapapa Holiday Park",
+      name: "Whakapapa Eco Escape",
       price: 240,
-      valid: {
-        minGuest:1,
-        minNight:1,
-        maxNight:15
-      },
+      minGuest:1,
+      maxGuest:4,
+      minNight:1,
+      maxNight:15,
       amenities: ["kitchen","Shared lounge"],
-      img: "whakapapa1.jpg",
+      img: "whakapapaHouse.jpg",
       add: "State Highway 48 Whakapapa Village, Mt Ruapehu, Tongariro National Park 3951",
       lat: -41.289816,
       lng: 174.779321,
@@ -156,16 +151,15 @@ var locations = [
     accom: [
       {
       id:1,
-      type:"hostel",
-      name: "Whakapapa Holiday Park",
+      type:"Hostel",
+      name: "Riverstone Backpackers",
       price: 30,
-      valid: {
-        minGuest:1,
-        minNight:1,
-        maxNight:10
-      },
+      minGuest:1,
+      maxGuest:1,
+      minNight:1,
+      maxNight:10,
       amenities: ["kitchen","Shared lounge"],
-      img: "whakapapa1.jpg",
+      img: "turangiHostel.jpg",
       add: "State Highway 48 Whakapapa Village, Mt Ruapehu, Tongariro National Park 3951",
       lat: -41.289816,
       lng: 174.779321,
@@ -174,16 +168,15 @@ var locations = [
     {
       id:2,
       type:"Motel",
-      name: "Whakapapa Holiday Park",
+      name: "Turangi Bridge Motel",
       price: 90,
-      valid: {
-        minGuest:2,
-        minNight:3,
-        maxNight:10
-      },
+      minGuest:2,
+      maxGuest:4,
+      minNight:3,
+      maxNight:10,
       amenities: ["kitchen","Shared lounge"],
-      img: "whakapapa1.jpg",
-      add: "State Highway 48 Whakapapa Village, Mt Ruapehu, Tongariro National Park 3951",
+      img: "turangiMotel.jpg",
+      add: "4600 State Highway 1, 3334 Turangi, New Zealand",
       lat: -41.289816,
       lng: 174.779321,
       description:"A 32 bed lodge and several tent sites are also available, all bookable online.",
@@ -191,16 +184,15 @@ var locations = [
     {
       id:3,
       type:"Hotel",
-      name: "Whakapapa Holiday Park",
-      price: 90,
-      valid: {
-        minGuest:1,
-        minNight:1,
-        maxNight:5
-      },
+      name: "Turangi Braxmere Hotel",
+      price: ,
+      minGuest:1,
+      maxGuest:2,
+      minNight:1,
+      maxNight:5,
       amenities: ["kitchen","Shared lounge"],
-      img: "whakapapa1.jpg",
-      add: "State Highway 48 Whakapapa Village, Mt Ruapehu, Tongariro National Park 3951",
+      img: "turangiHotel.jpg",
+      add: "88 Waihi Road, 3381 Turangi, New Zealand",
       lat: -41.289816,
       lng: 174.779321,
       description:"A 32 bed lodge and several tent sites are also available, all bookable online.",
@@ -208,16 +200,15 @@ var locations = [
     {
       id:4,
       type:"House",
-      name: "Whakapapa Holiday Park",
-      price: 90,
-      valid: {
-        minGuest:1,
-        minNight:1,
-        maxNight:15
-      },
+      name: "Turangi Pihanga View",
+      price: 240,
+      minGuest:1,
+      maxGuest:4,
+      minNight:1,
+      maxNight:15,
       amenities: ["kitchen","Shared lounge"],
-      img: "whakapapa1.jpg",
-      add: "State Highway 48 Whakapapa Village, Mt Ruapehu, Tongariro National Park 3951",
+      img: "turangiHouse.jpg",
+      add: "Pihanga View, 3381 Turangi, New Zealand",
       lat: -41.289816,
       lng: 174.779321,
       description:"A 32 bed lodge and several tent sites are also available, all bookable online.",
@@ -229,17 +220,16 @@ var locations = [
     accom:[
       {
       id:1,
-      type:"hostel",
-      name: "LKNZ Lodge",
+      type:"Hostel",
+      name: "Ohakune LKNZ Lodge",
       price: 30,
-      valid: {
-        minGuest: 1,
-        minNight:1,
-        maxNight:10
-      },
+      minGuest: 1,
+      maxGuest:1,
+      minNight:1,
+      maxNight:10,
       amenities: ["kitchen","Shared lounge"],
       img: "ohakuneHostel.jpg",
-      add: "State Highway 48 Whakapapa Village, Mt Ruapehu, Tongariro National Park 3951",
+      add: "1 Rata Street, 4265 Ohakune, New Zealand",
       lat: -41.289816,
       lng: 174.779321,
       description:"A 32 bed lodge and several tent sites are also available, all bookable online.",
@@ -247,15 +237,14 @@ var locations = [
     {
       id:2,
       type:"Motel",
-      name: "Ohakune Court Motel",
+      name: "Central Motel & Lodge",
       price: 90,
-      valid: {
-        minGuest: 2,
-        minNight:3,
-        maxNight:10
-      },
+      minGuest: 2,
+      maxGuest:4,
+      minNight:3,
+      maxNight:10,
       img: "ohakuneMotel.jpg",
-      add: "State Highway 48 Whakapapa Village, Mt Ruapehu, Tongariro National Park 3951",
+      add: "2 Moore Street, 4625 Ohakune, New Zealand",
       lat: -41.289816,
       lng: 174.779321,
       description:"A 32 bed lodge and several tent sites are also available, all bookable online.",
@@ -265,13 +254,12 @@ var locations = [
       type:"Hotel",
       name: "Tussock Grove Boutique Hotel",
       price: 157,
-      valid: {
-        minGuest: 1,
-        minNight:1,
-        maxNight:5
-      },
+      minGuest: 1,
+      maxGuest:2,
+      minNight:1,
+      maxNight:5,
       img: "ohakuneHotel.jpg",
-      add: "State Highway 48 Whakapapa Village, Mt Ruapehu, Tongariro National Park 3951",
+      add: "3 Karo Street, 4660 Ohakune, New Zealand",
       lat: -41.289816,
       lng: 174.779321,
       description:"A 32 bed lodge and several tent sites are also available, all bookable online.",
@@ -279,15 +267,14 @@ var locations = [
     {
       id:4,
       type:"House",
-      name: "Ohakune-Matata",
+      name: "Ohakune vue pour deux",
       price: 240,
-      valid: {
-        minGuest:1,
-        minNight:1,
-        maxNight:15
-      },
+      minGuest:1,
+      maxGuest:4,
+      minNight:1,
+      maxNight:15,
       img: "ohakuneHouse.jpg",
-      add: "2 Snowmass Drive, 4625 Ohakune, New Zealand",
+      add: "Tainui St, 4625 Ohakune, New Zealand",
       lat: -41.289816,
       lng: 174.779321,
         description:"A 32 bed lodge and several tent sites are also available, all bookable online.",
@@ -295,82 +282,126 @@ var locations = [
     ]
   }
 ];
+var filerArray = [];
 
 var option = $("#inputGroupSelect01");
 option.change(function(){
+  filerArray = [];
   if (option.val()==1) {
-    $("#cardsList").html('');
-    var nationalArray = [];
     for (var i = 0; i < locations.length; i++) {
       if (locations[i].placeName == 'national') {
-        nationalArray.push(locations[i])
+        filerArray.push(locations[i]);
       }
     }
-    itemCard(nationalArray);
   } else if (option.val()==2) {
-    $("#cardsList").html('');
-    var whakapapaArray = [];
     for (var i = 0; i < locations.length; i++) {
       if (locations[i].placeName == 'whakapapa') {
-        console.log('whakapapa');
-        whakapapaArray.push(locations[i])
+        filerArray.push(locations[i]);
       }
     }
-    itemCard(whakapapaArray);
   } else if (option.val()==3) {
-    $("#cardsList").html('');
-    var turangiArray = [];
     for (var i = 0; i < locations.length; i++) {
       if (locations[i].placeName == 'turangi') {
-        console.log('turangi');
-        turangiArray.push(locations[i])
+        filerArray.push(locations[i]);
       }
     }
-    itemCard(turangiArray);
   } else if (option.val()==4) {
-    $("#cardsList").html('');
-    var ohakuneArray = [];
     for (var i = 0; i < locations.length; i++) {
       if (locations[i].placeName == 'ohakune') {
-        console.log('ohakune');
-        ohakuneArray.push(locations[i])
+        filerArray.push(locations[i]);
       }
     }
-    itemCard(ohakuneArray);
   }
-
 });
 
-
-function itemCard(inputArray){
-  var accom = inputArray[0].accom;
+function itemCard(finalFilterArray){
+  cardsList.innerHTML='';
+  console.log(finalFilterArray);
+  var accom = finalFilterArray;
     for (var i = 0; i < accom.length; i++) {
-        var itemCard ='<div class="col-12 col-sm-6 col-md-3 mb-3 text-center">';
-          itemCard += '<div class="card itemThumb itemThumb2" style="width: 18rem; height:28rem;" data-id="'+accom[i].id+'">';
-          itemCard += '<img src ="img/'+accom[i].img+'"class="img-img-top img-thumbnail" alt="">';
+        var itemCard ='<div class=" text-center my-1">';
+          itemCard += '<div class="card " style="width: 16rem;" data-id="'+accom[i].id+'">';
+          itemCard += '<img src ="img/'+accom[i].img+'"class="img-img-top img-thumbnail" style="height: 12rem;" alt="">';
+            var typeColour = getTypeColour(accom[i].type);
+            console.log(typeColour);
+           itemCard += '<span class ="badge badge-'+typeColour+' mr-1">'+accom[i].type+'</span>';
             itemCard += '<div class="card-body">';
               itemCard += '<h5 class="card-title">'+accom[i].name+'</h5>';
               itemCard += '<h5 class="card-title">NZD'+accom[i].price+'/night</h5>';
-                itemCard +=  '<p class="card-text">Minimum Guest:'+accom[i].minGuest+', Minimum Night:'+accom[i].minNight+', Maximum night:'+accom[i].maxNight+'</p>';
-                  itemCard +=  '<a href="#" class="seeMore btn btn-outline-info btn-block">SEE MORE</a>';
-                  itemCard +=  '<a href="#" class="bookNow btn btn-info btn-block">BOOK NOW</a>';
+                itemCard +=  '<p class="card-text">Minimum Guest:'+accom[i].minGuest+' Maximum Guest:'+accom[i].maxGuest+' Minimum Night:'+accom[i].minNight+' Maximum night:'+accom[i].maxNight+'</p>';
+                  itemCard +=  '<a href="#" class="seeMore seeMore btn btn-outline-info btn-block">SEE MORE</a>';
+                  itemCard +=  '<a href="#" class="bookNow bookNow btn btn-info btn-block">BOOK NOW</a>';
             itemCard +='</div>';
           itemCard +='</div>';
         itemCard +='</div>';
 
         cardsList.innerHTML += itemCard;
     };
+    $(".bookNow").click(function(){
+      var diff2 = calculate();
+      var accom = finalFilterArray;
+      for (var i = 0; i < accom.length; i++) {
+        var getPrice = accom[0].price;
+        console.log(getPrice);
+      }
+      var totalCost = diff2 * getPrice;
+      var pay =["Credit Card","Debit Card","Cash","Others"];
+      var breakfast =["Yes","No"];
+      Swal.mixin({
+        input: 'text',
+        confirmButtonText: 'Next &rarr;',
+        showCancelButton: true,
+        progressSteps: ['1', '2', '3']
+      }).queue([
+        {
+          title: 'Total NZD '+totalCost,
+          html: 'For '+diff2+' Nights',
+          input: 'select',
+          inputPlaceholder: 'Breakfast Option',
+          animation:'slide-from-top',
+          inputOptions: breakfast
+        },
+        {
+          title: 'Payment Type',
+          input: 'select',
+          animation:'slide-from-top',
+          inputOptions: pay
+
+        },
+        {
+          title: 'Do you want to get confirmation email?',
+          input: 'email',
+          inputPlaceholder: 'Enter your email address'
+        }
+      ]).then((result) => {
+        if (result.value) {
+          Swal.fire({
+            type:'success',
+            title: 'Done!',
+            confirmButtonText: 'Thank You!'
+          })
+        }
+      })
+
+    });
+    $(".seeMore").click(function(){
+      $("#cardsList").hide();
+      $("#details").show();
+
+    });
+};
+
+function getTypeColour(type){
+  if (type === 'Hostel') {
+      return 'primary';
+  } else if (type === 'Motel') {
+      return 'success';
+  } else if (type=== 'Hotel') {
+      return 'danger';
+  } else {
+      return 'warning';
+  }
 }
-
-$("#search").click(function(){
-  $("#cardsList").show();
-  $(".banner").hide();
-  calculate();
-
-
-});
-
-
 
 
 //// datepicker setting //////
@@ -408,9 +439,45 @@ function calculate() {
     if (d1 && d2) {
       diff = Math.round(Math.abs((d2.getTime() - d1.getTime())/(oneDay)));
     }
-    console.log(diff);
+    return diff;
 }
 
+$("#search").click(function(){
+  $(".banner").hide();
+  $("#cardsList").show();
+  var diff2 = calculate();
+  console.log(diff2);
+    var guest = parseInt($("input[name='quant[1]']").val());
+    console.log(guest);
+
+  var date =[];
+  var final = [];
+  for (var i = 0; i < filerArray[0].accom.length; i++) {
+    if (diff2 >= filerArray[0].accom[i].minNight  && diff2 <= filerArray[0].accom[i].maxNight) {
+      date.push(filerArray[0].accom[i]);
+    }
+  }
+   for (var j = 0; j < date.length; j++) {
+      if (guest >= date[j].minGuest && guest <= date[j].maxGuest) {
+        final.push(date[j]);
+      }
+  }
+  itemCard(final);
+});
+
+$(document).ready(function(){
+  $("#owl1").owlCarousel({
+      loop: true,
+      autoWide: true,
+      center: true,
+      autoplay: true,
+      nav: true,
+      dots: true,
+      stagePadding: 0,
+      margin: 100,
+      lazyLoad: true
+  });
+});
 
 
 ////// Login Form //////
